389d36f47c3bbada0b9d792412d7ff57a245abaf
Divyashree <divyashreedwivedi@icloud.com>
2020-08-01 18:58:20 +0530

    Too lazy to comment

---
+++ Basics/arithematic_operators.py
@@ -0,0 +1,27 @@
+x = 5
+y = 3
+
+z = x+y
+z1 = x-y
+z2 = x/y
+z3 = x//y
+z4 = x*y
+z5 = x**y
+z6 = x % y 
+z7 = -z
+z8 = +z 
+print(f"Addition is {z}")
+print(f"Substraction is {z1}")
+print(f"Division is {z2}")
+print(f"Integer Division is {z3}")
+print(f"Multiplication is {z4}")
+print(f"Exponential is {z5}")
+print(f"Remainder or modulo is {z6}")
+print(f"Urinary negative is {z7}")
+print(f"Urinary  positive is {z8}")
+
+
+
+
+
+
