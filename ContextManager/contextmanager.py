da2b717099e91efcb43cf4ce45b4f416f35a3d95
Divyashree <divyashreedwivedi@icloud.com>
2020-08-11 16:58:15 +0530

    ContextManager

---
+++ ContextManager/contextmanager.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+''' 
+A context manager is a Python object that is able to act as a control structure when used after the "with" statement. They manage the context for a caller. That means performing some kind of set up, yielding control back to the caller, and then wrapping up when the caller is done.
+
+'''
