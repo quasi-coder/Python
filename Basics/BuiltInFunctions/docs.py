c546c04a2495b7ee7a0b7fd1c64f5390f521b3cf
Divyashree <divyashreedwivedi@icloud.com>
2020-09-02 11:23:50 +0530

    lamda function

---
+++ Basics/BuiltInFunctions/docs.py
@@ -0,0 +1,17 @@
+#import collections
+#print(map.__doc__)
+#print(collections.__doc__)
+
+def myFunction(arg1, arg2=None):
+    """myFunction(arg1, arg2=None) --> Doesn't do anything, just prints.
+
+    Parameters:
+    arg1: first argument. Whatever you feel like passing
+    arg2: second argument. Defaults to None
+    """
+    print(arg1,arg2)
+
+def main():
+    print(myFunction.__doc__)
+
+if __name__=='__main__':main()
